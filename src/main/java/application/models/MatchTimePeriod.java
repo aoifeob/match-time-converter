ea5e26b542c22b50f9d64851712a519c2fa6c685
aoifeob <obriea21@tcd.ie>
2019-08-17 01:43:20 +0100

    add initial test cases

---
+++ src/main/java/application/models/MatchTimePeriod.java
@@ -0,0 +1,50 @@
+package application.models;
+
+public class MatchTimePeriod {
+
+    private String periodShortForm;
+    private String periodLongForm;
+    private String minTimeStamp;
+    private String maxTimeStamp;
+    private boolean extraTimeAllowed;
+
+    public String getPeriodShortForm() {
+        return periodShortForm;
+    }
+
+    public void setPeriodShortForm(String periodShortForm) {
+        this.periodShortForm = periodShortForm;
+    }
+
+    public String getPeriodLongForm() {
+        return periodLongForm;
+    }
+
+    public void setPeriodLongForm(String periodLongForm) {
+        this.periodLongForm = periodLongForm;
+    }
+
+    public String getMinTimeStamp() {
+        return minTimeStamp;
+    }
+
+    public void setMinTimeStamp(String minTimeStamp) {
+        this.minTimeStamp = minTimeStamp;
+    }
+
+    public String getMaxTimeStamp() {
+        return maxTimeStamp;
+    }
+
+    public void setMaxTimeStamp(String maxTimeStamp) {
+        this.maxTimeStamp = maxTimeStamp;
+    }
+
+    public boolean isExtraTimeAllowed() {
+        return extraTimeAllowed;
+    }
+
+    public void setExtraTimeAllowed(boolean extraTimeAllowed) {
+        this.extraTimeAllowed = extraTimeAllowed;
+    }
+}
--- src/test/integration/java/application/InvalidInputsIT.java
+++ src/test/integration/java/application/InvalidInputsIT.java
@@ -1,4 +1,14 @@
 package application;
 
 public class InvalidInputsIT {
+
+    //Test 1: Test negative number timestamp returns invalid
+
+    //Test 2: Test incorrect period returns invalid
+
+    //Test 3: Test incorrect positive number format returns invalid
+
+    //Test 4: Test invalid file path returns invalid
+
+    //Test 5: Test additional characters in input returns invalid
 }
